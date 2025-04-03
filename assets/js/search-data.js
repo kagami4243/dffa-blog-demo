3f9a78dcf21a9fa388e2c80cf7f853d5f54b100b
kagami4243 <99196178+kagami4243@users.noreply.github.com>
2025-04-03 11:10:22 +0000

    Deploying to gh-pages from @ kagami4243/dffa-blog-demo@cf67017ee08785895275981ad510cf468d044466 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-dffa",
+    title: "dffa",
     section: "Navigation",
     handler: () => {
       window.location.href = "/dffa-blog-demo/";
@@ -17,7 +17,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/dffa-blog-demo/";
+            window.location.href = "/dffa-blog-demo/blog/2025/dffa-blog-demo/";
           
         },
       },{id: "post-a-post-with-plotly-js",
